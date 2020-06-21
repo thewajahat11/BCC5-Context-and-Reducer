69de5997bfd8bcac0deae2fbe2a0a28b0bc92079
Rana Wajahat <RanaWajahat@users.noreply.github.com>
2020-06-21 19:22:30 +0500

    Created context and reducer

---
+++ src/Child2.js
@@ -0,0 +1,17 @@
+import React, { useReducer } from 'react';
+import CounterReducer from './CounterReducer'
+
+const Child2 = () => {
+
+    let [state, dispatch] = useReducer(CounterReducer, 1)
+
+    return(
+        <div>
+            <h2>This is second child using Counter Reducer</h2>
+            <h3>Value of reducer is: {state}</h3>
+            <button onClick={()=>dispatch('INCREMENT')}>Increment Reducer</button>
+        </div>
+    )
+}
+
+export default Child2;
